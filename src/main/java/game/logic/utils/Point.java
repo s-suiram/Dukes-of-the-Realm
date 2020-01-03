3f8fc7182e9a993136866859d8f518b561d1c821
ange <angewarz@gmail.com>
2020-01-03 18:52:46 +0100

    point and rect

--- src/main/java/game/logic/utils/Point.java
+++ src/main/java/game/logic/utils/Point.java
@@ -1,31 +1,135 @@
 package game.logic.utils;
 
 import java.io.Serializable;
+import java.util.Objects;
 
+/**
+ * Defines a point in 2d space.
+ */
 public class Point implements Serializable {
+
     public int x;
     public int y;
 
+    /**
+     * Instantiates a new Point.
+     *
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public <T extends Number> Point(T x, T y) {
         this.x = x.intValue();
         this.y = y.intValue();
     }
 
+    /**
+     * Instantiates a new Point.
+     */
     public Point() {
         this.x = 0;
         this.y = 0;
     }
 
+    /**
+     * Instantiates a new Point.
+     *
+     * @param p the point to takes coordinates from.
+     */
     public Point(Point p) {
         this(p.x, p.y);
     }
 
-    public <T extends Number> void setLocation(T x, T y) {
+    /**
+     * set the location of this point.
+     *
+     * @param <T> a number
+     * @param x   the x position
+     * @param y   the y position
+     * @return this for chaining methods
+     */
+    public <T extends Number> Point setLocation(T x, T y) {
         this.x = x.intValue();
         this.y = y.intValue();
+        return this;
     }
 
-    public void setLocation(Point p) {
+    /**
+     * set the location of this point.
+     *
+     * @param p the point to copy the location from
+     * @return this for chaining methods
+     */
+    public Point setLocation(Point p) {
         setLocation(p.x, p.y);
+        return this;
+    }
+
+    /**
+     * adds one point to this one
+     *
+     * @param p the point to add
+     * @return this for chaining methods
+     */
+    public Point add(Point p) {
+        this.x += p.x;
+        this.y += p.y;
+        return this;
+    }
+
+    /**
+     * subs one point to this one
+     *
+     * @param p the point to sub
+     * @return this for chaining methods
+     */
+    public Point sub(Point p) {
+        this.x -= p.x;
+        this.y -= p.y;
+        return this;
+    }
+
+
+    /**
+     * compute the euclidean distance between two points.
+     * the point calling the method is considered the starting point
+     *
+     * @param p the point to compute distance to
+     * @return a new point containing the x and y distances
+     */
+    public Point euclideanDist(Point p) {
+        return new Point(p.x - x, p.y - y);
+    }
+
+    /**
+     * takes the negative of both coordinates.
+     *
+     * @return this for chaining methods
+     */
+    public Point neg(){
+        x = -x;
+        y = -y;
+        return this;
+    }
+
+    /**
+     * copy this point into another.
+     *
+     * @return a copy of this point
+     */
+    public Point cpy() {
+        return new Point(x, y);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/main/java/game/logic/utils/Rectangle.java
+++ src/main/java/game/logic/utils/Rectangle.java
@@ -2,45 +2,149 @@ package game.logic.utils;
 
 import java.io.Serializable;
 
+/**
+ * defines a 2d rectangle.
+ */
 public class Rectangle implements Serializable {
+
     public int x;
     public int y;
-    public int width;
-    public int height;
-    public int maxX;
-    public int maxY;
+    private int width;
+    private int height;
 
+    /**
+     * Instantiates a new Rectangle.
+     *
+     * @param x the x
+     * @param y the y
+     * @param w the w
+     * @param h the h
+     */
     public <T extends Number> Rectangle(T x, T y, T w, T h) {
+        if( w.intValue() < 0 || h.intValue() < 0)
+            throw new IllegalArgumentException("The width or height of a rectangle should not be negative.");
+
         this.x = x.intValue();
         this.y = y.intValue();
         this.width = w.intValue();
         this.height = h.intValue();
-        this.maxX = x.intValue() + w.intValue();
-        this.maxY = y.intValue() + h.intValue();
     }
 
+    /**
+     * Instantiates a new Rectangle.
+     *
+     * @param r the rectangle
+     */
+    public Rectangle(Rectangle r){
+        this(r.x, r.y, r.width, r.height);
+    }
+
+    /**
+     * Instantiates a new Rectangle.
+     *
+     * @param topLeft  the top left point
+     * @param botRight the bottom right point
+     */
+    public Rectangle(Point topLeft, Point botRight) {
+        this(topLeft.x, topLeft.y,  botRight.x - topLeft.x,  botRight.y - topLeft.y);
+    }
+
+    /**
+     * checks if a rectangle intersects another
+     *
+     * @param r the rectangle
+     * @return true if the two intersects
+     */
     public boolean intersects(Rectangle r) {
         if (r == null) return false;
-        return r.maxX > x && r.maxY > y && r.x < maxX && r.y < maxY;
+        return r.getMaxX() > x && r.getMaxY() > y && r.x < getMaxX() && r.y < getMaxY();
     }
 
+    /**
+     * Gets width.
+     *
+     * @return the width
+     */
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Sets width.
+     *
+     * @param width the width
+     */
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    /**
+     * Gets height.
+     *
+     * @return the height
+     */
+    public int getHeight() {
+        return height;
+    }
+
+    /**
+     * Sets height.
+     *
+     * @param height the height
+     */
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    /**
+     * computes the maxX coordinate
+     *
+     * @return maxX int
+     */
+    public int getMaxX(){
+        return x + width;
+    }
+
+    /**
+     * computes the maxY coordinate
+     *
+     * @return maxY int
+     */
+    public int getMaxY(){
+        return y + width;
+    }
+
+    /**
+     * Checks if this rectangle contains a point
+     *
+     * @param cx the x coordinate
+     * @param cy the y coordinate
+     * @return true if this rectangle contains the point
+     */
     public boolean contains(int cx, int cy) {
-        int tw = this.width;
-        int th = this.height;
-        if ((tw | th) < 0) {
+        if ((width | height) < 0) {
             // At least one of the dimensions is negative...
             return false;
         }
         // Note: if either dimension is zero, tests below must return false...
-        int tx = this.x;
-        int ty = this.y;
-        if (cx < tx || cy < ty) {
+
+        if (cx < x || cy < y) {
             return false;
         }
-        tw += tx;
-        th += ty;
+
+        int mx = getMaxX();
+        int my = getMaxY();
         //    overflow || intersect
-        return ((tw < tx || tw > cx) &&
-                (th < ty || th > cy));
+        return ((mx < x || mx > cx) && (my < y || my > cy));
+    }
+
+    /**
+     * Checks if this rectangle contains a point
+     *
+     * @param p the point
+     * @return true if this rectangle contains the point
+     */
+    public boolean contains(Point p) {
+        return contains(p.x, p.y);
     }
 }
